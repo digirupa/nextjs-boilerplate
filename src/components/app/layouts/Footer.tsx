4b4776f3a289ced33a02496f0e4b61f06cc3325f
Wahyu Ridiansyah <wahyuridiansyah@gmail.com>
2024-06-30 01:02:47 +0700

    style: improving responsive in footer & change language

--- src/components/app/layouts/Footer.tsx
+++ src/components/app/layouts/Footer.tsx
@@ -1,4 +1,3 @@
-import { format } from 'date-fns'
 import ChangeLanguage from '@/components/app/inputs/ChangeLanguage'
 import ChangeTheme from '../inputs/ChangeTheme'
 
@@ -7,11 +6,11 @@ const Footer = () => {
     <footer className='bg-background border-t text-center'>
       <div className='p-2'>
         <div className='flex flex-wrap items-center gap-1'>
-          <div className='flex gap-1 md:justify-center max-[392px]:mx-auto'>
+          <div className='flex gap-1'>
             <ChangeTheme />
             <ChangeLanguage />
           </div>
-          <p className='text-muted-foreground ml-auto max-[392px]:mr-auto' suppressHydrationWarning>
+          <p className='text-muted-foreground ml-auto' suppressHydrationWarning>
             Made in 🇮🇩 by <a href='https://github.com/digirupa'> Digirupa</a>
           </p>
         </div>
