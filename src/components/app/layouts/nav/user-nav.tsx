90fb2f3c744116c25dac305a6cd002c8f96fd171
Wahyu Ridiansyah <wahyuridiansyah@gmail.com>
2024-07-03 18:00:07 +0700

    feat: changes on full name

--- src/components/app/layouts/nav/user-nav.tsx
+++ src/components/app/layouts/nav/user-nav.tsx
@@ -31,13 +31,13 @@ export function UserNav() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className='w-56' align='end' forceMount>
-        <DropdownMenuLabel className='font-normal'>
+        <DropdownMenuLabel className='font-normal md:hidden'>
           <div className='flex flex-col space-y-1'>
-            <p className='text-sm font-medium leading-none'>{'session.user?.name'}</p>
-            <p className='text-xs leading-none text-muted-foreground'>{'session.user?.email'}</p>
+            <p className='text-sm font-medium leading-none'>{account?.full_name}</p>
+            <p className='text-xs leading-none text-muted-foreground'>{account.email}</p>
           </div>
         </DropdownMenuLabel>
-        <DropdownMenuSeparator />
+        <DropdownMenuSeparator className='md:hidden' />
         <DropdownMenuGroup>
           <DropdownMenuItem>
             Profile
